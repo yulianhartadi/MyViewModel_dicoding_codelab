9b926cefffa6dda66fce07ca91a176721fe852fa
RawzaDigital <rawzadigital@gmail.com>
2019-08-26 20:35:26 +0700

    add MyButton class, bg_button.xml, bg_button_disable.xml

--- app/src/main/java/com/rdstudio/myviewmodel/MyButton.java
+++ app/src/main/java/com/rdstudio/myviewmodel/MyButton.java
@@ -2,24 +2,47 @@ package com.rdstudio.myviewmodel;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 
+import androidx.core.content.ContextCompat;
+
+import static android.view.Gravity.CENTER;
+
 public class MyButton extends androidx.appcompat.widget.AppCompatButton {
+
+    private Drawable enabledBackground, disabledBackground;
+    private int textColor;
+
     public MyButton(Context context) {
         super(context);
+        init();
     }
 
     public MyButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public MyButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     // Use onDraw from View class to change Button shape
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        setBackground(isEnabled() ? enabledBackground : disabledBackground);
+        setTextColor(textColor);
+        setTextSize(12.f);
+        setGravity(CENTER);
+        setText(isEnabled() ? "Submit" : "Isi Dulu");
+    }
+
+    private void init(){
+        textColor = ContextCompat.getColor(getContext(), android.R.color.background_light);
+        enabledBackground = getResources().getDrawable(R.drawable.bg_button);
+        disabledBackground = getResources().getDrawable(R.drawable.bg_button_disable);
     }
 }
